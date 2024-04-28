8d81a64fb2655c905711259f78cf41d07ff6c033
TTT <sf>
2024-04-27 17:03:28 -0700

    update widget to display price info

--- extension/content-script.js
+++ extension/content-script.js
@@ -13,7 +13,7 @@ function initWidget() {
         
         <div class="si-text-container">
         <div class="si-header">
-        Daniel Widget
+        Austin Widget
         <svg class="si-leaf-icon" width="12" height="13" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M12.2199 2.69133C12.2137 2.5843 12.1684 2.48329 12.0925 2.40748C12.0167 2.33167 11.9157 2.28633 11.8087 2.28008C7.64533 2.03563 4.31049 3.28906 2.88861 5.64063C2.39575 6.44503 2.15288 7.37769 2.1908 8.32031C2.21555 8.9231 2.3382 9.51785 2.55393 10.0813C2.5666 10.1159 2.58785 10.1468 2.61568 10.1711C2.64352 10.1953 2.67704 10.2121 2.71312 10.2199C2.74921 10.2278 2.78668 10.2263 2.82205 10.2157C2.85742 10.2051 2.88954 10.1858 2.91541 10.1595L7.56385 5.43992C7.6045 5.39928 7.65275 5.36703 7.70586 5.34503C7.75897 5.32304 7.81589 5.31171 7.87338 5.31171C7.93086 5.31171 7.98779 5.32304 8.0409 5.34503C8.09401 5.36703 8.14226 5.39928 8.18291 5.43992C8.22356 5.48057 8.2558 5.52883 8.2778 5.58194C8.2998 5.63505 8.31112 5.69197 8.31112 5.74946C8.31112 5.80694 8.2998 5.86386 8.2778 5.91697C8.2558 5.97008 8.22356 6.01834 8.18291 6.05899L3.10299 11.2155L2.32697 11.9915C2.24627 12.0701 2.19809 12.1761 2.19201 12.2886C2.18593 12.4011 2.2224 12.5117 2.29416 12.5985C2.33346 12.644 2.38173 12.681 2.43594 12.707C2.49015 12.733 2.54914 12.7476 2.60923 12.7498C2.66933 12.752 2.72923 12.7418 2.7852 12.7198C2.84118 12.6978 2.89202 12.6645 2.93455 12.622L3.85275 11.7038C4.62604 12.0779 5.40643 12.2819 6.18025 12.3092C6.24114 12.3114 6.30184 12.3125 6.36236 12.3125C7.2436 12.3148 8.10812 12.072 8.8594 11.6114C11.211 10.1895 12.4649 6.85524 12.2199 2.69133Z" fill="#1BB333"></path>
         </svg>
@@ -32,20 +32,22 @@ function initWidget() {
         </div>
         </div>
         </div>
+        <div class="si-widget-container>
+          <div class="event-button" title="Event Info">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" style="width:60px;" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z"/></svg>
+            </div>
 
-        <div class="event-button" title="Event Info">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" id="event"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M16 13h-3c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-3c0-.55-.45-1-1-1zm0-10v1H8V3c0-.55-.45-1-1-1s-1 .45-1 1v1H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-1V3c0-.55-.45-1-1-1s-1 .45-1 1zm2 17H6c-.55 0-1-.45-1-1V9h14v10c0 .55-.45 1-1 1z"></path></svg>
-        <span>Event Info <span>
+            <span>Event Info <span>
+          </div>
+
+          <div class="cart-button" title="Cart Info">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" style="width:60px;" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+            </div>
+            <span>Cart Info<span>
+          </div>
         </div>
-
-        <div class="cart-button" title="Cart Info">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" enable-background="new 0 0 2000 2000" viewBox="0 0 2000 2000" id="shopping-speed"><linearGradient id="a" x1="673.815" x2="1758.511" y1="867.155" y2="853.068" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><path fill="url(#a)" d="M1539.82,1129.49l86.87-536.97H754.83l132.62,536.97H1539.82z M1591.64,622.35l-77.21,477.31H910.81
-        L792.93,622.35H1591.64z"></path><linearGradient id="b" x1="674.161" x2="1758.857" y1="893.813" y2="879.726" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><polygon fill="url(#b)" points="732.65 592.66 576.72 590.76 576.36 620.58 709.2 622.21 847.95 1186.27 1539.99 1186.89 1540.02 1157.06 871.34 1156.45"></polygon><linearGradient id="c" x1="679.061" x2="1763.757" y1="1271.082" y2="1256.995" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><path fill="url(#c)" d="M1448.82,1200.18c-33.58,0-60.9,27.32-60.9,60.9c0,33.58,27.32,60.9,60.9,60.9
-        c33.58,0,60.9-27.33,60.9-60.9C1509.72,1227.5,1482.4,1200.18,1448.82,1200.18z M1448.82,1292.15c-17.13,0-31.07-13.94-31.07-31.07
-        s13.94-31.07,31.07-31.07c17.13,0,31.07,13.94,31.07,31.07S1465.95,1292.15,1448.82,1292.15z"></path><linearGradient id="d" x1="678.974" x2="1763.67" y1="1264.4" y2="1250.313" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><path fill="url(#d)" d="M934.22,1200.18c-33.58,0-60.9,27.32-60.9,60.9c0,33.58,27.32,60.9,60.9,60.9
-        c33.58,0,60.9-27.33,60.9-60.9C995.13,1227.5,967.81,1200.18,934.22,1200.18z M934.22,1292.15c-17.13,0-31.07-13.94-31.07-31.07
-        s13.94-31.07,31.07-31.07s31.07,13.94,31.07,31.07S951.36,1292.15,934.22,1292.15z"></path><linearGradient id="e" x1="673.886" x2="1758.582" y1="872.604" y2="858.517" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><rect width="223.42" height="29.83" x="484.22" y="858.7" fill="url(#e)"></rect><linearGradient id="f" x1="675.977" x2="1760.673" y1="1033.631" y2="1019.545" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><rect width="277.65" height="29.83" x="489.3" y="1019.34" fill="url(#f)"></rect><linearGradient id="g" x1="674.915" x2="1759.61" y1="951.824" y2="937.738" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><rect width="277.65" height="29.83" x="373.31" y="939.02" fill="url(#g)"></rect><linearGradient id="h" x1="677.033" x2="1761.728" y1="1114.914" y2="1100.827" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><rect width="163.3" height="29.83" x="621.52" y="1099.66" fill="url(#h)"></rect><linearGradient id="i" x1="674.947" x2="1759.643" y1="954.351" y2="940.264" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fec95f"></stop><stop offset="1" stop-color="#f04c4d"></stop></linearGradient><rect width="56.68" height="29.83" x="678.35" y="939.02" fill="url(#i)"></rect></svg>
-        <span>Cart Info<span>
         </div>
         
     <div>
@@ -70,7 +72,7 @@ function scrapeProductImages() {
   for (let i = 0; i < images.length; i++) {
     imageUrls.push(images[i].src);
   }
-  
+
   const path = location.pathname.split("/");
   const productName = path[path.length - 1];
   const data = {
@@ -113,24 +115,69 @@ function getCartTotal() {
 
 function createCartInfoModal() {
   // Create container for the modal
+  const modalElement = document.createElement("div");
   const modalContainer = document.createElement("div");
+
   modalContainer.id = "cartInfoModal";
   modalContainer.style.cssText =
     "display:block; position:fixed; top:50%; left:50%; transform:translate(-50%, -50%); background:white; z-index:1000; padding:20px; border-radius:5px;";
 
   // Adding inner HTML content
   modalContainer.innerHTML = `
-  <h2>Cart Details</h2>
+  <h2>Cart Information</h2>
+  
   <div id="cartItemsContainer"></div>
   <div id="totalPriceContainer"></div>
-  <button id="closeButton">Close</button>
+  <button id="closeButton" class="button" style="margin-top:20px;" >Close</button>
   `;
 
-  document.body.appendChild(modalContainer);
+  const overlayElement = document.createElement("div");
+  overlayElement.style.cssText = `position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 999;" > </div>`;
+  overlayElement.innerHTML = " ";
+
+  modalElement.append(overlayElement);
+  modalElement.append(modalContainer);
+
+  document.body.appendChild(modalElement);
 
   // Hook up the close event to remove the modal
   document.getElementById("closeButton").addEventListener("click", function () {
     modalContainer.remove();
+    overlayElement.remove();
+  });
+}
+
+function createEventInfoModal() {
+  // Create container for the modal
+  const modalElement = document.createElement("div");
+  const modalContainer = document.createElement("div");
+
+  modalContainer.id = "cartInfoModal";
+  modalContainer.style.cssText =
+    "display:block; position:fixed; top:50%; left:50%; transform:translate(-50%, -50%); background:white; z-index:1000; padding:20px; border-radius:5px;";
+
+  // Adding inner HTML content
+  modalContainer.innerHTML = `
+  <h2>Cart Information</h2>
+  
+  <div id="cartItemsContainer"></div>
+  <div id="totalPriceContainer"></div>
+  <button id="closeButton" class="button" style="margin-top:20px;" >Close</button>
+  `;
+
+  const overlayElement = document.createElement("div");
+  overlayElement.style.cssText = `position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 999;" > </div>`;
+  overlayElement.innerHTML = " ";
+
+  modalElement.append(overlayElement);
+  modalElement.append(modalContainer);
+
+  document.body.appendChild(modalElement);
+
+  // Hook up the close event to remove the modal
+  document.getElementById("closeButton").addEventListener("click", function () {
+    modalContainer.remove();
+    overlayElement.remove();
   });
 }
 
@@ -139,23 +186,84 @@ function displayCartInfo() {
 
   const modal = document.getElementById("cartInfoModal");
   const itemsContainer = document.getElementById("cartItemsContainer");
-  const totalPriceContainer = document.getElementById("totalPriceContainer");
 
   const items = getCartItems();
-  itemsContainer.innerHTML = items
-    .map((item) => `<p>${item.name}: ${item.price}</p>`)
-    .join("");
-  totalPriceContainer.innerHTML = `<strong>Total: ${getCartTotal()}</strong>`;
+
+  // const items = getCartItems();s
+  // itemsContainer.innerHTML = items
+  //   .map((item) => `<p>${item.name}: ${item.price}</p>`)
+  //   .join("");
+  // totalPriceContainer.innerHTML = `<strong>Total: ${getCartTotal()}</strong>`;
+
+  itemsContainer.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${items
+        .map((item) => `<tr><td>${item.name}</td><td>${item.price}</td></tr>`)
+        .join("")}
+      <tr>
+        <td>Total</td><td>${getCartTotal()}</td>
+      </tr>
+    </tbody>
+  </table>
+  `;
 
   modal.style.display = "block";
 }
 
+function displayEventInfo() {
+  createEventInfoModal(); // Ensure modal creation is prior to data insertion.
+
+  const modal = document.getElementById("cartInfoModal");
+  const itemsContainer = document.getElementById("cartItemsContainer");
+
+  const items = getCartItems();
+
+  // const items = getCartItems();s
+  // itemsContainer.innerHTML = items
+  //   .map((item) => `<p>${item.name}: ${item.price}</p>`)
+  //   .join("");
+  // totalPriceContainer.innerHTML = `<strong>Total: ${getCartTotal()}</strong>`;
+
+  itemsContainer.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${items
+        .map((item) => `<tr><td>${item.name}</td><td>${item.price}</td></tr>`)
+        .join("")}
+      <tr>
+        <td>Total</td><td>${getCartTotal()}</td>
+      </tr>
+    </tbody>
+  </table>
+  `;
+
+  modal.style.display = "block";
+
+}
 // All Event Handlers
 function attachEventHandlers() {
   configureWidgetCheckbox();
   document
     .querySelector(".cart-button")
     .addEventListener("click", displayCartInfo);
+
+    
+  document
+  .querySelector(".event-button")
+  .addEventListener("click", displayEventInfo);
 }
 
 setTimeout(async () => {
